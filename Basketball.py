6ceb9ffc06cf96615d2963dce1746bd7b902a683
Shivaniii15 <st22421@ormiston.school.nz>
2023-03-28 00:39:40 +1300

    Update Basketball.py

---
+++ Basketball.py
@@ -0,0 +1,16 @@
+class basketball:
+    def __init__ (self, name,goals,games):
+        self.name = name #self.name is now an attribute of the basketball class
+        self.goals = goals
+        self.games = games
+
+    
+    def speak(self):
+        print(self.name, self.goals, self.games)
+
+Jeana = basketball("Jeana", 10, 20)
+Jessie = basketball("Jessie", 5, 10)
+
+
+Jeana.speak() #print "jeana"
+Jessie.speak() #print "jessie"
