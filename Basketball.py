5b7477ec0f28e857af95b5af1ae30b9a7b03eb59
Shivaniii15 <st22421@ormiston.school.nz>
2023-03-28 20:27:22 +1300

    Update Basketball.py

--- Basketball.py
+++ Basketball.py
@@ -3,22 +3,18 @@ class basketball:
         self.name = name #self.name is now an attribute of the basketball class
         self.goals = goals
         self.games = games
+#arugements here: name, games and goals
 
+    def calculate_average(self): #function to create average gaoals per game
+        if self.games == 0:
+            return 0.0
+        return self.goals / self.games
 
-    def calculate_average(goals,games):
-        goals = sum(goals)
-        games = len(games)
-        average = goals / games
-        return average
-
-
-    def speak(self):
-        print(self.name, "scored", self.goals,"goals in", self.games, "games and her average number goals per game were")
-
+    def display_average(self):
+        avg_goals = self.calculate_average()
+        print(self.name, "scored", self.goals,"goals in", self.games, "games and her average number goals per game were", self.display_average)
 
 Jeana = basketball("Jeana", 10, 20)
 Jessie = basketball("Jessie", 5, 10)
-
-
-Jeana.speak() #print "jeana"
-Jessie.speak() #print "jessie"
+Jeana.display_average()
+Jessie.display_average()
