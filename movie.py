6082688bece29b13c89f92acd92905ba0b31b8c7
Shivaniii15 <st22421@ormiston.school.nz>
2023-03-28 20:56:49 +1300

    Update movie.py

--- movie.py
+++ movie.py
@@ -9,14 +9,14 @@ class movie:
     def calculate_net_profit(self): #new function to calculate net profit
         if self.revenue == 0:
             return 0.0
-        return self.expenses / self.revenue #expenses/revenue=net profit
+        return self.revenue - self.expenses #expenses/revenue=net profit
     
     def display_net_profit(self):
         net_profit=self.calculate_net_profit()
         print (self.title, "is a", self.genre, "movie which has an expenses of", self.expenses, "revenue of", self.revenue, "and their net profit earned is", net_profit)
 
-DDLJ=movie(title="DDLJ", genre="romance", expenses=10000, revenue=50000)
-K3G= movie(title="K3G", genre="romcom", expenses=250000, revenue=800000)
+DDLJ=movie(title="DDLJ", genre="romance", expenses=1000, revenue=50000)
+K3G= movie(title="K3G", genre="romcom", expenses=25000, revenue=800000)
 DDLJ.display_net_profit()
 K3G.display_net_profit()
 
